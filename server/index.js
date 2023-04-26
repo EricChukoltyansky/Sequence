ae80ba72b3bd4d3b37c257044aeca684c0e9839c
EricChukoltyansky <kazohelmet@gmail.com>
2023-04-26 18:04:51 +0300

    redis config

--- server/index.js
+++ server/index.js
@@ -3,11 +3,37 @@ const http = require("http");
 const cors = require("cors");
 const app = express();
 const { Server } = require("socket.io");
+const redis = require("redis");
 
 app.use(cors());
 app.use(express);
 
 const server = http.createServer(app);
+
+let redisClient = redis.createClient({
+  url: process.env.REDIS_URL,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
+redisClient.connect();
+
+redisClient.on("connect", () => {
+  console.log("Redis client connected");
+});
+
+redisClient.on("error", (err) => {
+  console.log("Something went wrong " + err);
+});
+
+redisClient.on("message", (channel, message) => {
+  console.log("Message: " + message + " on channel: " + channel + " is arrive!");
+});
+
+redisClient.subscribe("arm");
+redisClient.subscribe("switch");
+redisClient.subscribe("rewind");
+redisClient.subscribe("clearAll");
+redisClient.subscribe("BPM");
 
 const io = new Server(server, {
   cors: {
